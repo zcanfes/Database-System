543d825b06fa075d9f4ed800003788eca72a86e3
zcanfes <zcanfes@gmail.com>
2021-06-10 20:21:05 +0300

    insert table

--- django/dtbank/urls.py
+++ django/dtbank/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import create_table, create_trigger
+from .views import create_table, create_trigger, insert_table
 
 urlpatterns = [
     path('api/create_table', create_table.index, name='create_table'),
     path('api/create_trigger', create_trigger.index, name='create_trigger'),
+    path('api/insert_table', insert_table.index, name='insert_tabler'),
 ]
